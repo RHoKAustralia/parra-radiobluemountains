09421bf8d72db8ff9d9d3dd7518ea6c0f7bab4b6
alrosot <alrosot@yahoo.com.br>
2016-11-26 15:43:26 +1100

    Update calendarMerger.js
    Making it dynamic (going through the week)

--- calendarMerger.js
+++ calendarMerger.js
@@ -4,7 +4,23 @@ var consolidatedCalendar = CalendarApp.getCalendarById("0s6n1i40gorq5m4uad57ec0d
 var crnCalendar = CalendarApp.getCalendarById("1cs29j3n6ebb3n8d4pa98flhoc@group.calendar.google.com");
 var rbmCalendar = CalendarApp.getCalendarById("7l3qtj8h2dud0jthrmm4cp6hss@group.calendar.google.com");
 
+function addDay(date, days){  
+  var newDate = new Date( date.getTime() + days * 86400000 );
+  return newDate;
+}
 
+function goThroughTheWeek(){
+  
+  var agendaDay = new Date();
+  agendaDay.setHours(0);
+  agendaDay.setMinutes(0);
+  agendaDay.setSeconds(0);
+  agendaDay.setMilliseconds(0);
+  
+  for(var i = 0;i<7;i++){
+    mergeCalendars(addDay(agendaDay,i));
+  }
+}
 
 function copyFromCrn(description, startTime, endTime){
   var crnEvents = crnCalendar.getEvents(startTime, endTime);
@@ -24,11 +40,8 @@ function copyFromCrn(description, startTime, endTime){
 }
 
 // Copies everything from RMB, find the free gaps and then fill them with CRN programs (sometimes partialy)
-function mergeCalendars() {
+function mergeCalendars(agendaDay) {
   var rbmCalendar = CalendarApp.getCalendarById("7l3qtj8h2dud0jthrmm4cp6hss@group.calendar.google.com");
-    
-  var agendaDay = new Date('Mon Nov 20 2016 00:00:00 GMT+1100 (AEDT)');
-  
   
   // cleaning up consolidated calendar  
   var consolidatedEvents = consolidatedCalendar.getEventsForDay(agendaDay);
@@ -50,7 +63,7 @@ function mergeCalendars() {
     }     
     startFreeTime = event.getEndTime();
     if(i == rbmEvents.length-1){
-      finishTime = new Date('Mon Nov 21 2016 00:00:00 GMT+1100 (AEDT)')
+      finishTime = addDay(agendaDay, 1);
     } else { 
       finishTime = rbmEvents[i+1].getStartTime();               
     }
@@ -59,9 +72,5 @@ function mergeCalendars() {
     
     consolidatedCalendar.createEvent(event.getTitle(), event.getStartTime(), event.getEndTime());
   }
-  
-  
-  
-  
   
 }
